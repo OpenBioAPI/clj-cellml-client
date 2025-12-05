82c3c3e304fbbc0efd9e8215aee6cf6cc7e4e3be
Schmoho <38255275+Schmoho@users.noreply.github.com>
2025-12-05 12:06:36 +0100

    Update dependencies in project.clj

--- project.clj
+++ project.clj
@@ -9,8 +9,7 @@ __Documentation:__ https://aucklandphysiomerepository.readthedocs.io/en/latest/w
   :url "https://models.cellml.org"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [metosin/spec-tools "0.7.0"]
-                 [clj-http "3.8.0"]
-                 [orchestra "2017.11.12-1"]
-                 [cheshire "5.8.0"]])
+  :dependencies [[metosin/spec-tools "0.7.0"]
+                 [clj-http/clj-http "3.8.0"]
+                 [orchestra/orchestra "2017.11.12-1"]
+                 [cheshire/cheshire "6.1.0"]])
